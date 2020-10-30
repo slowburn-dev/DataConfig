961c2084340abff120a0f1abd69172e84ffa9e9e
jagt <jagt@live.com>
2020-10-31 00:57:32 +0800

    pretty print numeric

--- DataConfig/Source/DataConfigCore/Private/DataConfig/Writer/DcPrettyPrintWriter.cpp
+++ DataConfig/Source/DataConfigCore/Private/DataConfig/Writer/DcPrettyPrintWriter.cpp
@@ -19,7 +19,7 @@ FDcResult FDcPrettyPrintWriter::WriteNext(EDcDataEntry Next)
 
 FDcResult FDcPrettyPrintWriter::WriteBool(bool Value)
 {
-	Output.Logf(TEXT("%s- bool: %d"), *Indent, Value);
+	Output.Logf(TEXT("%s- bool: %s"), *Indent, Value ? TEXT("true") : TEXT("false"));
 	return DcOk();
 }
 
@@ -96,6 +96,66 @@ FDcResult FDcPrettyPrintWriter::WriteReference(UObject* Value)
 {
 	check(Value);
 	Output.Logf(TEXT("%s- ref: %d"), *Indent, Value->GetUniqueID());
+	return DcOk();
+}
+
+FDcResult FDcPrettyPrintWriter::WriteInt8(const int8& Value)
+{
+	Output.Logf(TEXT("%s- int8: \"%d\""), *Indent, Value);
+	return DcOk();
+}
+
+FDcResult FDcPrettyPrintWriter::WriteInt16(const int16& Value)
+{
+	Output.Logf(TEXT("%s- int16: \"%d\""), *Indent, Value);
+	return DcOk();
+}
+
+FDcResult FDcPrettyPrintWriter::WriteInt32(const int32& Value)
+{
+	Output.Logf(TEXT("%s- int32: \"%d\""), *Indent, Value);
+	return DcOk();
+}
+
+FDcResult FDcPrettyPrintWriter::WriteInt64(const int64& Value)
+{
+	Output.Logf(TEXT("%s- int64: \"%d\""), *Indent, Value);
+	return DcOk();
+}
+
+FDcResult FDcPrettyPrintWriter::WriteUInt8(const uint8& Value)
+{
+	Output.Logf(TEXT("%s- uint8: \"%o\""), *Indent, Value);
+	return DcOk();
+}
+
+FDcResult FDcPrettyPrintWriter::WriteUInt16(const uint16& Value)
+{
+	Output.Logf(TEXT("%s- uint16: \"%o\""), *Indent, Value);
+	return DcOk();
+}
+
+FDcResult FDcPrettyPrintWriter::WriteUInt32(const uint32& Value)
+{
+	Output.Logf(TEXT("%s- uint32: \"%o\""), *Indent, Value);
+	return DcOk();
+}
+
+FDcResult FDcPrettyPrintWriter::WriteUInt64(const uint64& Value)
+{
+	Output.Logf(TEXT("%s- uint64: \"%o\""), *Indent, Value);
+	return DcOk();
+}
+
+FDcResult FDcPrettyPrintWriter::WriteFloat(const float& Value)
+{
+	Output.Logf(TEXT("%s- float: \"%f\""), *Indent, Value);
+	return DcOk();
+}
+
+FDcResult FDcPrettyPrintWriter::WriteDouble(const double& Value)
+{
+	Output.Logf(TEXT("%s- double: \"%f\""), *Indent, Value);
 	return DcOk();
 }
 
