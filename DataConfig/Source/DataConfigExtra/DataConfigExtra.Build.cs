7b1c093cd99e2571788669acdae8a2ec5a3d7889
jagt <jagt@live.com>
2021-03-06 20:28:19 +0800

    working DcJsonConverter

--- DataConfig/Source/DataConfigExtra/DataConfigExtra.Build.cs
+++ DataConfig/Source/DataConfigExtra/DataConfigExtra.Build.cs
@@ -10,11 +10,11 @@ public class DataConfigExtra : ModuleRules
 
 		PublicDependencyModuleNames.AddRange(
 			new string[] {
-			//	still need these 2 for link to pass, wtf?
 			"Core",
 			"CoreUObject",
 			"PropertyPath",
-
+			"Json",
+			"JsonUtilities",
 			"DataConfigCore",
 			});
 
