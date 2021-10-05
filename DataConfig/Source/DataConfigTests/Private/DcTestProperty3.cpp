aca58cbdd95fed4514153859a0c36169f754c2b0
jagt <jagt@live.com>
2021-10-05 16:10:57 +0800

    ue5 LargeWorldCoords vector tests pass

--- DataConfig/Source/DataConfigTests/Private/DcTestProperty3.cpp
+++ DataConfig/Source/DataConfigTests/Private/DcTestProperty3.cpp
@@ -24,8 +24,8 @@ DC_TEST("DataConfig.Core.Property.TObjectPtr")
 	FDcPropertyDatum SourceDatum(TBaseStructure<FTestUninitializedScriptStructMembersTest>::Get(), &Source);
 	FDcPropertyDatum DestDatum(TBaseStructure<FTestUninitializedScriptStructMembersTest>::Get(), &Dest);
 
-	UTEST_OK("FDcTestStruct4 roundtrip", DcTestPropertyRoundtrip(this, SourceDatum, DestDatum));
-	UTEST_OK("FDcTestStruct4 roundtrip equal", DcAutomationUtils::TestReadDatumEqual(SourceDatum, DestDatum));
+	UTEST_OK("FTestUninitializedScriptStructMembersTest roundtrip", DcTestPropertyRoundtrip(this, SourceDatum, DestDatum));
+	UTEST_OK("FTestUninitializedScriptStructMembersTest roundtrip equal", DcAutomationUtils::TestReadDatumEqual(SourceDatum, DestDatum));
 
 	//	compile checks
 	if (0)
@@ -37,6 +37,29 @@ DC_TEST("DataConfig.Core.Property.TObjectPtr")
 		FDcWriter Writer;
 		Writer.WriteTObjectPtr(ObjPtr).Ok();
 	}
+
+	return true;
+}
+
+DC_TEST("DataConfig.Core.Property.FLargeWorldCoordinates")
+{
+	FVector Source;
+
+	Source.X = 1.5f;
+	Source.Y = 1.75f;
+	Source.Z = 1.875f;
+
+	FVector Dest;
+
+	FDcPropertyDatum SourceDatum(TBaseStructure<FVector>::Get(), &Source);
+	FDcPropertyDatum DestDatum(TBaseStructure<FVector>::Get(), &Dest);
+
+	UTEST_OK("FVector roundtrip", DcTestPropertyRoundtrip(this, SourceDatum, DestDatum));
+	UTEST_OK("FVector roundtrip equal", DcAutomationUtils::TestReadDatumEqual(SourceDatum, DestDatum));
+
+	UTEST_TRUE("FVector X", Dest.X == 1.5f);
+	UTEST_TRUE("FVector Y", Dest.Y == 1.75f);
+	UTEST_TRUE("FVector Z", Dest.Z == 1.875f);
 
 	return true;
 }
